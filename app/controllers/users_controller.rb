59479b231f3e91a1b621d48fc76e725dadbd16cc
James Rae <jrae151515@yahoo.co.uk>
2022-10-09 15:53:55 +0100

    Added edit functionality with error messages appearing if form can't be saved due to blank fields

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,38 @@
 class UsersController < ApplicationController
-    def new
-        @user = User.new
-    end
+  before_action :set_user, only: %i[ show edit update destroy ]
+
+  def show
+  end
+
+  def new
+    @user = User.new
+  end
  
-    def create
-        @user = User.new(user_params)
+  def create
+    @user = User.new(user_params)
 
-        if @user.save
-          redirect_to new_user_path
-        else
-          render :new, status: :unprocessable_entity
-        end
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new, status: :unprocessable_entity
     end
+  end
 
-    private
-
-    def user_params
-        params.require(:user).permit(:email,:username,:password)
+  def update
+    if @user.update(user_params)
+      redirect_to user_url(@user), notice: "User was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity 
     end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:email,:username,:password)
+  end
 end
